cb96869d0df62f237889d407b36f65c4f8c58688
CEDILEY BERNARDINO DE OLIVEIRA <57760738+Cediley@users.noreply.github.com>
2021-11-23 15:33:37 -0300

    Exercio de Treino
    Realizando soma e media de N números, através da IDE Intellij

---
+++ Ex_Média/src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.Scanner;
+/*
+	Criando um programa, onde ele vai recerber 5 numeros
+	informar o maior deles
+	e a media de todos os 5
+	 */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numero;
+        int maior = 0;
+        int soma = 0;
+
+
+        int contador = 0;
+
+        do {
+            System.out.println("numero: ");
+            numero = scan.nextInt();
+
+            soma = soma + numero;
+            if (numero > maior) maior = numero;
+
+                    contador = contador + 1;
+
+        } while (contador < 5);
+
+        System.out.println("O maior numero é: " + maior);
+        System.out.println("A média deu: " + soma/5);
+
+    }
+}
