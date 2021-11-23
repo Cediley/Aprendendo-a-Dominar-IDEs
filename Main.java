50343a3d1edea2bbb2e9f7c9743f23142abc4c81
CEDILEY BERNARDINO DE OLIVEIRA <57760738+Cediley@users.noreply.github.com>
2021-11-23 16:17:32 -0300

    Exemplo de Exercicio
    Exercicio realizado para testar utilização de o metodo de repetição 'Do'

--- Main.java
+++ Main.java
@@ -1,37 +1,53 @@
 package com.company;
-
-import java.util.Scanner;
-/*
-	Criando um programa, onde ele vai recerber 5 numeros
-	informar o maior deles
-	e a media de todos os 5
+    /*
+	Programa que receberá N qauntidades de numeros inteiros
+	Calculará todos eles e mostrará a quantidade de numeros
+	pares e impares
 	 */
 
-class Ex_media {
+import java.util.Scanner;
 
-    public Ex_media(String[] args) {
+public class Main {
+
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int numero;
-        int maior = 0;
-        int soma = 0;
+        int quantNumeros; //Variavel que representa a quantidade de vezes que o laço se repita
+        int numero; //Variavel que recebe os numeros que serão avaliados e trabalhados no programa
+        int par = 0; //Variavel que representa a quantidade de numeros pares
+        int impar = 0; //Variavel que representa a quantidade de numeros impares
 
+        System.out.println("Qauntidade de Numeros: ");
+        quantNumeros = scan.nextInt();
 
-        int contador = 0;
-
+        int count = 0;
         do {
             System.out.println("numero: ");
             numero = scan.nextInt();
 
-            soma = soma + numero;
-            if (numero > maior) maior = numero;
+            if (numero % 2 == 0) par++;
+            // se a divisão po 2 do numero for igual a 0, add mais um valor a variavel 'par'
+            else impar++;
+            /* Se a divisão do numero não for igual a 0, add mais um valor a variavel impar
+            Obs: exatamente por isso hambas as variaveis começam com valor 0
+            por conta de no inicio do programa, ainda não temos nenhum valor na variavel 'numero'*/
+    /*
+    Quando entrar no loop 'Do', será pedido uma quantidade de numeros:
+    Esses numeros serão avaliados e divididos individualmente
+   "se"  divisão deste numero for igual a 0: nesse caso o numero será par
+   Caso contrario "então", o numero é impar
+     */
 
-                    contador = contador + 1;
+            count = count +1;
+        } while (count < quantNumeros);
+        /*
+        Quando o codigo chegar a execução do 'Do" e 'While' teremos inicio ao loop
+        Onde o contador 'count' deverá incicar quando o loop deve parar
+        Ao sujeitar a quantidade de numeros na variavel 'quantNumeros'
+         */
 
-        } while (contador < 5);
-
-        System.out.println("O maior numero é: " + maior);
-        System.out.println("A média deu: " + soma/5);
-
+        System.out.println("Qauntidade de Numeros Pares: " + par);
+        System.out.println("Quantidade de Numeros Impares: " + impar);
+        //Mostrando ou imprimindo os Resultados
     }
 }
