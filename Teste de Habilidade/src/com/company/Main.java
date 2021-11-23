a3ddf676f1b61605cdf0bb2a63f7a03735758ed0
CEDILEY BERNARDINO DE OLIVEIRA <57760738+Cediley@users.noreply.github.com>
2021-11-23 16:13:56 -0300

    Add files via upload

---
+++ Teste de Habilidade/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*treinando declarações de dados
+	tipos de dados e trabalhando com operadores relacionais
+	 */
+        byte a = 3;
+        short b = 9;
+        int c = 13;
+        float c2 = 4.5F;
+        double d = 2.5d;
+
+        String s1 = "Fulano";
+        String s2 = "Fulano";
+        String s3 = "Ciclano";
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+
+        System.out.println(a * b - c);
+
+    }
+}
